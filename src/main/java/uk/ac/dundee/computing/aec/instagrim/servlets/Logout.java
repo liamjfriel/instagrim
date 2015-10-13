f13ff74296e619e5a8c3c5addbc0188ac847d9a4
liamjfriel <liamjfriel@gmail.com>
2015-10-13 21:41:23 +0100

    Added Logout servlet.

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Logout.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.dundee.computing.aec.instagrim.servlets;
+
+import com.datastax.driver.core.Cluster;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
+import uk.ac.dundee.computing.aec.instagrim.models.User;
+import uk.ac.dundee.computing.aec.instagrim.stores.LoggedIn;
+
+/**
+ *
+ * @author Liam Friel
+ */
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet {
+
+
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session=request.getSession();
+        System.out.println("Session in servlet "+session);
+        
+        LoggedIn lg= new LoggedIn();
+        lg.setLoginState(false); //Set login state to false, meaning we aren't logged in
+        lg.setUsername(null); //Set the username to null because we are no longer logged in
+          
+        session.setAttribute("LoggedIn", lg);
+        System.out.println("Session in servlet "+session);
+        RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
+	rd.forward(request,response); 
+    }
+        
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "This servlet changes the state of the bean to reflect the decision to logout taken by the user.";
+    }// </editor-fold>
+
+
+}
